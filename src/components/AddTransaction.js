b0a4d490635ced455becb2c89b6b31bc1fe797cf
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2021-06-29 05:25:58 +0530

    set the state using createContext hook

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  let [text, setText] = useState("");
+  let [amount, setAmount] = useState(0);
   return (
     <div>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
-          <label for="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <label>Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
-          <label for="amount">
+          <label>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
